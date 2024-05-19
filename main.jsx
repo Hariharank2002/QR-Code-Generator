63e14bc94140e31cb71890fdce76c24afb09f078
K.HARIHARAN <139669880+Hariharank2002@users.noreply.github.com>
2024-05-19 18:18:46 +0530

    Update main.jsx

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './Qr.css'
+import { Qrgenrator } from './Qrgenrator'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Qrgenrator />
+  </React.StrictMode>,
+)
